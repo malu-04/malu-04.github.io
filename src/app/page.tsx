c7b2eae72279652f32dbfc9d58f3fe6ed683b7cd
Malumbo Mkandawire <mmalumbo@gmail.com>
2026-05-28 20:55:18 +0200

    refactor: clean up Home component by removing unnecessary content and social links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,38 +19,9 @@ function SocialLink({
 export default function Home() {
   return (
     <Container className="mt-9">
-      <div className="max-w-2xl">
-        <h4 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+      <h4 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
           Senior Software Engineer
         </h4>
-
-        <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400 text-justify">
-          Senior  Software  Engineer  with  7+  years  of  experience  in  designing,  developing,  testing,  and maintaining application systems in regulated and large-scale environments. Strong background in translating system requirements into efficient, secure, and scalable program code.
-        </p>
-
-        <p className="mt-2 text-base text-zinc-600 dark:text-zinc-400 text-justify">
-          Experienced in full software development lifecycle including algorithm design, coding, testing (unit and  integration),  debugging,  deployment,  and  documentation.  Proven  ability  to  build  reliable systems, implement security best practices, and support users through training and troubleshooting.
-        </p>
-
-        <div className="mt-6 flex gap-6">
-            <SocialLink href="#" aria-label="Follow on X" icon={XIcon} />
-            <SocialLink
-              href="#"
-              aria-label="Follow on Instagram"
-              icon={InstagramIcon}
-            />
-            <SocialLink
-              href="#"
-              aria-label="Follow on GitHub"
-              icon={GitHubIcon}
-            />
-            <SocialLink
-              href="#"
-              aria-label="Follow on LinkedIn"
-              icon={LinkedInIcon}
-            />
-          </div>
-      </div>
     </Container>
 
   )
